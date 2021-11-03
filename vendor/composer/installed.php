4edb3d2d43a6392e99c1a5fc6a00599d90ee38b2
r2020andx <raimonds.a@gmail.com>
2021-11-03 18:17:01 +0200

    Add vendor directory

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'abf211aff39b2baa379cd549d45383b82b452c67',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'abf211aff39b2baa379cd549d45383b82b452c67',
+            'dev_requirement' => false,
+        ),
+    ),
+);
